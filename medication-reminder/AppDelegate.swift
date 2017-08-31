2701da203334ecc9dd81f72e430389baf7e7b2d1
Francisco Campos <franciscocampos@Franciscos-Mac.local>
2017-08-31 16:00:11 -0700

    Completed POC Localhost REST communication succesfully and separated subfolders

--- medication-reminder/AppDelegate.swift
+++ medication-reminder/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //Getting medication data
+        Alamofire.request("http://localhost:9000/api/medications").responseJSON { response in
+            //Print to console for debug
+            // original URL request
+            print(response.request)
+            // server data
+            print(response.data)
+            // result of response serialization
+            print(response.result)
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
+        
         return true
     }
 
---
+++ medication-reminder/TableController.swift
@@ -0,0 +1,44 @@
+//
+//  TableController.swift
+//  medication-reminder
+//
+//  Created by Francisco Campos on 2017-08-31.
+//  Copyright © 2017 Vikas Gandhi. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class TableController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //Declare variables
+    let cellList = ["A", "B", "C"]
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return cellList.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = cellList[indexPath.row]
+        return cell
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
